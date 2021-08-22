c1f8f3b7e3c139c6a062e8562af82950642688ae
noob247365 <noob247365@gmail.com>
2021-08-22 16:00:39 -0400

    Overhauled Bones.cpp to add GLTF caching

--- include/Bones.h
+++ include/Bones.h
@@ -5,29 +5,66 @@
 
 #include "openvr_driver.h"
 
-const short NUM_BONES = 31;
+enum class HandSkeletonBone : vr::BoneIndex_t {
+  eBone_Root = 0,
+  eBone_Wrist,
+  eBone_Thumb0,
+  eBone_Thumb1,
+  eBone_Thumb2,
+  eBone_Thumb3,
+  eBone_IndexFinger0,
+  eBone_IndexFinger1,
+  eBone_IndexFinger2,
+  eBone_IndexFinger3,
+  eBone_IndexFinger4,
+  eBone_MiddleFinger0,
+  eBone_MiddleFinger1,
+  eBone_MiddleFinger2,
+  eBone_MiddleFinger3,
+  eBone_MiddleFinger4,
+  eBone_RingFinger0,
+  eBone_RingFinger1,
+  eBone_RingFinger2,
+  eBone_RingFinger3,
+  eBone_RingFinger4,
+  eBone_PinkyFinger0,
+  eBone_PinkyFinger1,
+  eBone_PinkyFinger2,
+  eBone_PinkyFinger3,
+  eBone_PinkyFinger4,
+  eBone_Aux_Thumb,
+  eBone_Aux_IndexFinger,
+  eBone_Aux_MiddleFinger,
+  eBone_Aux_RingFinger,
+  eBone_Aux_PinkyFinger,
+  eBone_Count
+};
+
+const short NUM_BONES = (short)HandSkeletonBone::eBone_Count;
+
+extern vr::VRBoneTransform_t rightOpenPose[NUM_BONES];
+extern vr::VRBoneTransform_t leftOpenPose[NUM_BONES];
 
 struct Transform_t {
-  Transform_t() : rotation({0.0f, 0.0f, 0.0f, 0.0f}), translation({0.0f, 0.0f, 0.0f}){};
+  Transform_t();
   std::array<float, 4> rotation;
   std::array<float, 3> translation;
 };
 
 struct AnimationData_t {
-  std::array<Transform_t, 2> transforms;
-  std::array<float, 2> times;
+  AnimationData_t();
+  Transform_t startTransform;
+  float startTime;
+  Transform_t endTransform;
+  float endTime;
 };
-
-extern vr::VRBoneTransform_t rightOpenPose[NUM_BONES];
-
-extern vr::VRBoneTransform_t leftOpenPose[NUM_BONES];
 
 class IModelManager {
  public:
   virtual bool Load() = 0;
 
-  virtual AnimationData_t GetAnimationDataByNodeIndex(const size_t& boneIndex, float f) const = 0;
-  virtual Transform_t GetTransformByNodeIndex(const size_t& boneIndex) const = 0;
+  virtual AnimationData_t GetAnimationDataByBoneIndex(const HandSkeletonBone& boneIndex, float f) const = 0;
+  virtual Transform_t GetTransformByBoneIndex(const HandSkeletonBone& boneIndex) const = 0;
 };
 
 class BoneAnimator {
@@ -36,7 +73,7 @@ class BoneAnimator {
   void ComputeSkeletonTransforms(vr::VRBoneTransform_t* skeleton, const std::array<float, 5>& flexion, bool rightHand);
 
  private:
-  vr::VRBoneTransform_t GetTransformForBone(const size_t boneIndex, const float f);
+  vr::VRBoneTransform_t GetTransformForBone(const HandSkeletonBone& boneIndex, const float f);
 
   std::string m_fileName;
   std::unique_ptr<IModelManager> m_modelManager;
