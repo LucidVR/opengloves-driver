be7f37357422b1e85978d0474d7b51659cadd5cd
danwillm <39023874+danwillm@users.noreply.github.com>
2022-04-19 18:24:36 +0100

    remove skeletal update error logs

--- src/DeviceDriver/DeviceDriver.cpp
+++ src/DeviceDriver/DeviceDriver.cpp
@@ -125,14 +125,8 @@ void DeviceDriver::StartDevice() {
   communicationManager_->BeginListener([&](VRInputData data) {
     try {
       boneAnimator_->ComputeSkeletonTransforms(handTransforms_, data, IsRightHand());
-      vr::EVRInputError err = vr::VRDriverInput()->UpdateSkeletonComponent(skeletalComponentHandle_, vr::VRSkeletalMotionRange_WithoutController, handTransforms_, NUM_BONES);
-      if (err != vr::EVRInputError::VRInputError_None) {
-        DriverLog("Error! %i", err);
-      }
-      err = vr::VRDriverInput()->UpdateSkeletonComponent(skeletalComponentHandle_, vr::VRSkeletalMotionRange_WithController, handTransforms_, NUM_BONES);
-      if (err != vr::EVRInputError::VRInputError_None) {
-        DriverLog("Error! %i", err);
-      }
+      vr::VRDriverInput()->UpdateSkeletonComponent(skeletalComponentHandle_, vr::VRSkeletalMotionRange_WithoutController, handTransforms_, NUM_BONES);
+      vr::VRDriverInput()->UpdateSkeletonComponent(skeletalComponentHandle_, vr::VRSkeletalMotionRange_WithController, handTransforms_, NUM_BONES);
 
       HandleInput(data);
 
