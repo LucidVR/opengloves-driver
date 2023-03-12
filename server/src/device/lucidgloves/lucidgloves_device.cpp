9c759114e6d67c965d22345767f3883dd277b6d3
danwillm <39023874+danwillm@users.noreply.github.com>
2023-03-12 18:17:43 +0000

    attempted fixes for incorrect force feedback hand being activated

--- server/src/device/lucidgloves/lucidgloves_device.cpp
+++ server/src/device/lucidgloves/lucidgloves_device.cpp
@@ -10,19 +10,20 @@ class LucidglovesDevice::Impl {
  public:
   Impl(og::Hand hand, std::unique_ptr<ICommunicationManager> communication_manager)
       : hand_(hand), communication_manager_(std::move(communication_manager)) {
-    force_feedback_ = std::make_unique<InputForceFeedbackNamedPipe>(hand, [&](const ForceFeedbackCurlData &curl_data) {
-      og::Output output{};
+    force_feedback_ = std::make_unique<InputForceFeedbackNamedPipe>(hand_, [&](const ForceFeedbackCurlData &curl_data) {
 
-      og::OutputForceFeedbackData output_force_feedback_data{};
-      output_force_feedback_data.thumb = curl_data.thumb;
-      output_force_feedback_data.index = curl_data.index;
-      output_force_feedback_data.middle = curl_data.middle;
-      output_force_feedback_data.ring = curl_data.ring;
-      output_force_feedback_data.pinky = curl_data.pinky;
-
-      output.data.force_feedback_data = output_force_feedback_data;
-
-      output.type = og::kOutputData_Type_ForceFeedback;
+      const og::Output output = {
+          .type = og::kOutputData_Type_ForceFeedback,
+          .data = {
+              .force_feedback_data = {
+                  .thumb = curl_data.thumb,
+                  .index = curl_data.index,
+                  .middle = curl_data.middle,
+                  .ring = curl_data.ring,
+                  .pinky = curl_data.pinky
+              }
+          },
+      };
 
       communication_manager_->WriteOutput(output);
     });
@@ -60,7 +61,9 @@ class LucidglovesDevice::Impl {
 };
 
 LucidglovesDevice::LucidglovesDevice(og::DeviceConfiguration configuration, std::unique_ptr<ICommunicationManager> communication_manager)
-    : configuration_(std::move(configuration)), pImpl_(std::make_unique<Impl>(configuration_.hand, std::move(communication_manager))) {
+    : configuration_(std::move(configuration)) {
+  pImpl_ = std::make_unique<Impl>(configuration_.hand, std::move(communication_manager));
+
   OutputOSCServer::GetInstance();
 };
 
